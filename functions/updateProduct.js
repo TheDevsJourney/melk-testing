c09a472de20cec51bd3c5095d5ff9c5d91142c65
Timothy <thedevsjourney@gmail.com>
2020-08-30 14:21:02 -0500

    ff

--- functions/updateProduct.js
+++ functions/updateProduct.js
@@ -1,31 +1,31 @@
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`)
+// const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`)
 
-const statusCode = 200
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type",
-}
+// const statusCode = 200
+// const headers = {
+//   "Access-Control-Allow-Origin": "*",
+//   "Access-Control-Allow-Headers": "Content-Type",
+// }
 
-exports.handler = async event => {
-  const { sku, quantity } = JSON.parse(event.body)
+// exports.handler = async event => {
+//   const { sku, quantity } = JSON.parse(event.body)
 
-  let product
+//   let product
 
-  try {
-    product = await stripe.products.update(
-      sku.toString(), //Product.id  Do I need to change to string?
-      { metadata: { quantity: "0" }, active: "false" }
-    )
-  } catch (e) {
-    console.error(e.message)
-  }
+//   try {
+//     product = await stripe.products.update(
+//       sku,
+//       { metadata: { quantity: "0" }, active: "false" }
+//     )
+//   } catch (e) {
+//     console.error(e.message)
+//   }
 
-  return {
-    statusCode,
-    headers,
-    body: JSON.stringify({
-      status: "worked",
-      message: "Did it work?",
-    }),
-  }
-}
+//   return {
+//     statusCode,
+//     headers,
+//     body: JSON.stringify({
+//       status: "worked",
+//       message: "Did it work?",
+//     }),
+//   }
+// }
