586244e0b12d6d7635482e2d40c818b91a1d30fa
Timothy <thedevsjourney@gmail.com>
2020-08-30 00:08:55 -0500

    asdfffa

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
-import { useShoppingCart } from 'use-shopping-cart'
+import { useShoppingCart } from "use-shopping-cart"
 
 const buttonStyles = {
-  fontSize: '13px',
-  textAlign: 'center',
-  color: '#fff',
-  outline: 'none',
-  padding: '12px',
-  boxShadow: '2px 5px 10px rgba(0,0,0,.1)',
-  backgroundColor: 'rgb(255, 178, 56)',
-  borderRadius: '6px',
-  letterSpacing: '1.5px',
+  fontSize: "13px",
+  textAlign: "center",
+  color: "#fff",
+  outline: "none",
+  padding: "12px",
+  boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
+  backgroundColor: "rgb(255, 178, 56)",
+  borderRadius: "6px",
+  letterSpacing: "1.5px",
 }
 
 const Cart = () => {
@@ -39,7 +39,7 @@ const Cart = () => {
           redirectToCheckout()
         }}
       >
-        {loading ? 'Loading...' : 'Checkout'}
+        {loading ? "Loading..." : "Checkout"}
       </button>
       <button style={buttonStyles} onClick={clearCart}>
         Clear cart
--- src/utils/stripejs.js
+++
@@ -1,9 +0,0 @@
-// import { loadStripe } from "@stripe/stripe-js"
-// let stripePromise
-// const getStripe = () => {
-//   if (!stripePromise) {
-//     stripePromise = loadStripe("")
-//   }
-//   return stripePromise
-// }
-// export default getStripe
