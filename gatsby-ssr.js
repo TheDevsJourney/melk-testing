5ef012009f273a92010d00dd02f6feb8b40e84d1
Timothy <thedevsjourney@gmail.com>
2020-08-31 19:28:26 -0500

    asdf

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
-const React = require("react")
-const Layout = require("./src/components/Layout").default
+// const React = require("react")
+// const Layout = require("./src/components/Layout").default
 
-// Wraps every page in a component
-exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
+// // Wraps every page in a component
+// exports.wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>
+// }
