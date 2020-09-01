6628f2b2cab01a91f056d32d482c938ef6025007
Timothy <thedevsjourney@gmail.com>
2020-08-31 22:42:28 -0500

    asdf

--- functions/updateProduct.js
+++ functions/updateProduct.js
@@ -1,10 +1,10 @@
 const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`)
 
-// const statusCode = 200
-// const headers = {
-//   "Access-Control-Allow-Origin": "*",
-//   "Access-Control-Allow-Headers": "Content-Type",
-// }
+const statusCode = 200
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+}
 
 // exports.handler = async event => {
 //   const { sku } = JSON.parse(event.body)
@@ -31,40 +31,25 @@ const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`)
 // }
 
 // Testing
-exports.handler = async ({ body, headers, event }) => {
+exports.handler = async event => {
   const { sku } = JSON.parse(event.body)
+
+  let product
   try {
-    // check the webhook to make sure it’s valid
-    const stripeEvent = stripe.webhooks.constructEvent(
-      body,
-      headers["stripe-signature"],
-      process.env.STRIPE_WEBHOOK_SECRET
-    )
+    product = await stripe.products.update(sku, {
+      metadata: { Quantity: "0" },
+      active: "false",
+    })
+  } catch (e) {
+    console.error(e.message)
+  }
 
-    // only do stuff if this is a successful Stripe Checkout purchase
-    if (stripeEvent.type === "charge.succeeded") {
-      let product
-
-      try {
-        product = await stripe.products.update(sku, {
-          metadata: { quantity: "0" },
-          active: "false",
-        })
-      } catch (e) {
-        console.error(e.message)
-      }
-    }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ received: true }),
-    }
-  } catch (err) {
-    console.log(`Stripe webhook failed with ${err}`)
-
-    return {
-      statusCode: 400,
-      body: `Webhook Error: ${err.message}`,
-    }
+  return {
+    statusCode,
+    headers,
+    body: JSON.stringify({
+      status: "worked",
+      message: "Did it work?",
+    }),
   }
 }
