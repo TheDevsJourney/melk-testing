4f0c66c6c1e47ef5c6714909ca594a68e194e44f
Timothy <thedevsjourney@gmail.com>
2020-09-01 19:15:09 -0500

    list

--- functions/getProducts.js
+++ functions/getProducts.js
@@ -1,5 +1,5 @@
 const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`)
 
 exports.handler = async () => {
-  return stripe.products.retrieve("prod_HrhMl8JQCWOdXp")
+  return stripe.products.list()
 }
