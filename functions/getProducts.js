f37a48c9ea035c370391720ce5de2b4dc0da6422
Timothy <thedevsjourney@gmail.com>
2020-09-01 19:30:50 -0500

    asdffdsa

--- functions/getProducts.js
+++ functions/getProducts.js
@@ -1,6 +1,14 @@
 const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`)
 
-exports.handler = async () => {
+exports.handler = async (event, context, callback) => {
   const products = await stripe.products.list()
-  return products
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(products),
+  }
+  callback(null, response)
 }
